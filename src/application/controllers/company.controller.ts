c48951ad72b1b0e4bf5d35dc13b61adc3384f4be
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-08-24 19:41:43 -0300

    feat: implementa CompanyController

---
+++ src/application/controllers/company.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { ICompanyService } from 'src/domain/interfaces/company.interface';
+import { CompanyDto, CreateCompanyDto } from '../dtos/company.dto';
+
+@Controller('company')
+export class CompanyController {
+  constructor(
+    @Inject('ICompanyService') private readonly companyService: ICompanyService,
+  ) {}
+  @Post('create')
+  async CreateCompanyDto(
+    @Body() createCompanyDto: CreateCompanyDto,
+  ): Promise<CompanyDto> {
+    return await this.companyService.createCompany(createCompanyDto);
+  }
+
+  @Get()
+  async getAllCompanies(): Promise<CompanyDto[]> {
+    return await this.companyService.findAllCompanies();
+  }
+
+  @Get(':id')
+  async getOneCompany(@Param() { id }: { id: string }): Promise<CompanyDto> {
+    return await this.companyService.findOneCompany(id);
+  }
+}
