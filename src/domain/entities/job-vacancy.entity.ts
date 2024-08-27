fb0ff95667ed6877cf48af2ec4be5826cb449b9a
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-08-27 13:34:39 -0300

    feat: cria JobVacancyEntity

---
+++ src/domain/entities/job-vacancy.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { CompanyEntity } from './company.entity';
+
+@Entity()
+export class JobVacancyEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar' })
+  title: string;
+
+  @Column({ type: 'varchar' })
+  description: string;
+
+  @Column({ type: 'varchar' })
+  @ManyToOne(() => CompanyEntity, (company) => company.job_vacancies)
+  companyId: string;
+}
