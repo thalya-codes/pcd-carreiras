8df02167dd422513e47969eb3d8cd22ce85eee87
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-08-27 13:45:21 -0300

    feat: provê e exporta job vacancy service

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyEntity } from './entities/company.entity';
 import { InfrastructureModule } from 'src/infrastructure/infrastructure.module';
 import { CompanyService } from './services/company.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JobVacancyEntity } from 'src/domain/entities/job-vacancy.entity';
+import { JobVacancyService } from 'src/domain/services/job-vacancy.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CompanyEntity]), InfrastructureModule],
-  providers: [{ provide: 'ICompanyService', useClass: CompanyService }],
-  exports: ['ICompanyService'],
+  imports: [
+    TypeOrmModule.forFeature([CompanyEntity, JobVacancyEntity]),
+    InfrastructureModule,
+  ],
+  providers: [
+    { provide: 'ICompanyService', useClass: CompanyService },
+    { provide: 'IJobVacancyService', useClass: JobVacancyService },
+  ],
+  exports: ['ICompanyService', 'IJobVacancyService'],
 })
 export class DomainModule {}
