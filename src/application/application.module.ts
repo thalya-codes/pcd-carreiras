7d95db1146437e051d01bdbaf3babe069e71d77a
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-08-27 13:46:40 -0300

    feat: adiciona JobVacancyController no ApplicationModule

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { JobVacancyController } from './controllers/job-vacancy.controller';
 import { CompanyController } from './controllers/company.controller';
 import { DomainModule } from 'src/domain/domain.module';
 @Module({
   imports: [DomainModule],
-  controllers: [CompanyController],
+  controllers: [CompanyController, JobVacancyController],
 })
 export class ApplicationModule {}
