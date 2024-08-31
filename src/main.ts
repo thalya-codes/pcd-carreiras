b2436dcc07ba8d21cc57afadac8a21a2f1402e67
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-08-31 15:49:01 -0300

    chore: ajusta setup endpoint

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/doc', app, document);
 
   await app.listen(3000);
 }
