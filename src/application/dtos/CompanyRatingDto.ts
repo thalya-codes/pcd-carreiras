6bc3e871ab3b4d88b091777e002467b1a8a00dc1
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-08-31 15:39:33 -0300

    docs: documenta dto CompanyRatingDto

--- src/application/dtos/CompanyRatingDto.ts
+++ src/application/dtos/CompanyRatingDto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CompanyRatingDto {
- id: string;
- name: string;
- avgRating: number;
+  @ApiProperty({ example: 'id' })
+  id: string;
+
+  @ApiProperty({ example: 'Nome da empresa' })
+  name: string;
+
+  @ApiProperty({ example: 'Pontuação da empresa' })
+  avgRating: number;
 }
