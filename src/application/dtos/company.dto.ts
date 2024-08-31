d4f10730c5dbdf0d1a42ae9235dde7b935e91a2b
Thalya Stéffany <87552041+thalya-codes@users.noreply.github.com>
2024-08-31 15:29:20 -0300

    docs: documenta endpoints da entity Company

--- src/application/dtos/company.dto.ts
+++ src/application/dtos/company.dto.ts
@@ -1,18 +1,31 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsArray, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class BaseCompanyDto {
+  @ApiProperty({
+    example: 'Nome empresa',
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiPropertyOptional({
+    example: 'Descrição',
+  })
   @IsOptional()
   @IsString()
   description?: string;
 
+  @ApiPropertyOptional({
+    example: 'id-vaga',
+  })
   @IsOptional()
   @IsArray()
   job_vacancies?: string[];
 
+  @ApiPropertyOptional({
+    example: 'id-feedback',
+  })
   @IsOptional()
   @IsArray()
   feedbacks?: string[];
